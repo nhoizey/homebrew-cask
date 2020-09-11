d0141656f195406ce37de993cee3a39e6f25d266
Nicolas Hoizey <nicolas@hoizey.com>
2020-09-11 15:37:01 +0200

    Add Subsmarine

---
+++ Casks/subsmarine.rb
@@ -0,0 +1,14 @@
+# Because it was removed from Homebrew official Casks
+# https://github.com/Homebrew/homebrew-cask/commit/19ad2b9e10417f95ccab12ee8031e2bea96ec13e
+cask 'subsmarine' do
+  version '1.3'
+  sha256 '17be7a606995cdb9dbfe083720b056160855b52cfedb7371e707b5d596926ff3'
+
+  # cwcbucket.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://cwcbucket.s3.amazonaws.com/subsmarine/subsmarine.app.#{version}.zip"
+  appcast 'https://www.cocoawithchurros.com/shine/appcast.php?id=7'
+  name 'SubsMarine'
+  homepage 'https://www.cocoawithchurros.com/subsmarine.php'
+
+  app 'SubsMarine.app'
+end
