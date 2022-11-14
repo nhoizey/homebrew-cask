f14977a5cb1b2567c7046da40b878c22c77272e8
Nicolas Hoizey <nicolas@hoizey.com>
2022-11-14 11:44:09 +0100

    Update screenflow9.rb

--- Casks/screenflow9.rb
+++ Casks/screenflow9.rb
@@ -1,9 +1,9 @@
-cask "screenflow" do
+cask "screenflow9" do
   version "9.0.7,31680"
   sha256 "242e46d893043c8fabf6367066e772e85c53fee489367936bb38c806e810c652"
 
   url "https://www.telestream.net/download-files/screenflow/#{version.before_comma.major_minor.dots_to_hyphens}/ScreenFlow-#{version.before_comma}.dmg"
-  name "ScreenFlow"
+  name "ScreenFlow 9"
   desc "Screen recording and video editing software"
   homepage "https://www.telestream.net/screenflow/"
 
